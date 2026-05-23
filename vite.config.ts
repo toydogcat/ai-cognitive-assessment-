412be087b830695372e82d34ecca6febd15cef70
toydogcat <toydogcat@gmail.com>
2026-05-24 02:44:43 +0800

    feat: set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ai-cognitive-assessment-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
